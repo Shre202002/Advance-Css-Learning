30fd804410edb88398d4d23ded30c2902fca46b6
Shre <sriyanshgupta24@gmail.com>
2022-12-08 13:34:44 +0530

    Learning Advance Css With All Its Property

---
+++ Concept/transitions/script.js
@@ -0,0 +1,9 @@
+let scale = document.querySelector('.scal');
+
+let button = document.querySelector('#btn');
+
+button.addEventListener('click',()=>{
+      scale.style.width = '80%';
+});
+
+
---
+++ Peatice/Progress_Slider/script.js
@@ -0,0 +1,9 @@
+let scale = document.querySelector('.progress');
+
+let button = document.querySelector('#ht', '#cs');
+
+
+button.addEventListener('click', () => {
+    scale.style.width = '90%';
+
+});
